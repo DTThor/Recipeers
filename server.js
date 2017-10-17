2602407e4708a5b0d36154962712f1903412ec76
dtthor <d.t.thorwaldson@gmail.com>
2017-10-17 13:05:21 -0500

    Bring in cloudinary in server

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const methodOverride = require('express-method-override');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser');
+const cloudinary = require('cloudinary-core');
+const cl = new cloudinary.Cloudinary({recipeers: "demo", secure: true});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
