3a716ae5272d9383329f4bb7aedf7ea351c4003e
Amanda Freeman <amandafreeman@Amandas-MacBook-Pro.local>
2017-10-17 10:15:40 -0500

    add morgan and cookies to server.js

--- server.js
+++ server.js
@@ -2,14 +2,28 @@
 const PORT = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const methodOverride = require('express-method-override');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
 //set up ejs
 app.set('view engine', 'ejs');
+
+//set up morgan
+app.use(morgan('dev'));
+
+//set up cookies
+app.use(cookieParser());
+app.set('trust proxy', 1) // trust first proxy
+app.use(cookieSession({
+  name: 'session',
+  keys: ['banana', 'kiwi', 'pineapple']
+}));
 
 //home page route
 app.get('/', (req, res) => {
