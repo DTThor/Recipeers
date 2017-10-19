96a3ae27dc868b0dcc489cf34ff29397f4d8b2d3
Michael Quiroz <cocomjolk@hotmail.com>
2017-10-18 19:15:13 -0500

    seeds modified

--- seeds/05favorites.js
+++ seeds/05favorites.js
@@ -4,12 +4,12 @@ exports.seed = function(knex, Promise) {
   return knex('favorites').del()
     .then(function () {
       // Inserts seed entries
-  /*     return knex('favorites').insert([
+       return knex('favorites').insert([
        {user_id: 1, favorite_recipe_id: 7},
-        {user_id: 2, favorite_recipe_id: 11 },
+        {user_id: 2, favorite_recipe_id: 3 },
         {user_id: 2, favorite_recipe_id: 10 },
         {user_id: 2, favorite_recipe_id: 9 },
-        {user_id: 3, favorite_recipe_id: 11 },
+        {user_id: 3, favorite_recipe_id: 2 },
         {user_id: 3, favorite_recipe_id: 1 },
         {user_id: 3, favorite_recipe_id: 3 },
         {user_id: 4, favorite_recipe_id: 1 },
@@ -18,12 +18,12 @@ exports.seed = function(knex, Promise) {
         {user_id: 6, favorite_recipe_id: 8},
         {user_id: 6, favorite_recipe_id: 5 },
         {user_id: 7, favorite_recipe_id: 4 },
-        {user_id: 7, favorite_recipe_id: 11 },
+        {user_id: 7, favorite_recipe_id: 2 },
         {user_id: 8, favorite_recipe_id: 8 },
         {user_id: 8, favorite_recipe_id: 9 },
         {user_id: 9, favorite_recipe_id: 1 },
-        {user_id: 9, favorite_recipe_id: 11 }
+        {user_id: 9, favorite_recipe_id: 2 }
 
-      ]); */
+      ]);
     });
 };
